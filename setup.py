34b71e991585f7ea5f727b3ddd3e30168020e102
Matthias Rozanski <matthias.rozanski@ifu.rwth-aachen.de>
2019-07-13 15:55:10 +0200

    now reads long_description from README.md for better PyPi display

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+from os import path
+
+# load long description from README.md
+setup_directory = path.abspath(path.dirname(__file__))
+with open(path.join(setup_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="pyloa",
@@ -13,9 +19,8 @@ setup(
     python_requires='>=3.5',
     install_requires=['matplotlib==3.0.3', 'scipy==1.2.1', 'tensorflow==1.13.1', 'tqdm==4.31.1', 'numpy==1.16.2'],
     description="PyLoa: Learning on-line Algorithms with Python",
-    long_description="A research repository for analyzing the performance of classic on-line algorithms vs modern "
-                     "Machine Learning, specifically Reinforcement Learning, approaches.",
-
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/pyloa/PyLoa",
     project_urls={
         "Bug Tracker": "https://github.com/pyloa/PyLoa/issues",

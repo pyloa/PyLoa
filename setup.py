5e20d0e4665ed4aa7acb21a560cba4a85b88ab64
Matthias Rozanski <matthias.rozanski@ifu.rwth-aachen.de>
2019-07-13 16:45:33 +0200

    added __classifier__, __install_requires__ and __version__ are now loaded from respecitive files

--- setup.py
+++ setup.py
@@ -1,14 +1,45 @@
 from setuptools import setup, find_packages
-from os import path
+import os
 
-# load long description from README.md
-setup_directory = path.abspath(path.dirname(__file__))
-with open(path.join(setup_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+# load info from files
+setup_directory = os.path.abspath(os.path.dirname(__file__))
+readme_file = os.path.join(setup_directory, 'README.md')
+version_file = os.path.join(setup_directory, 'pyloa', '_version.py')
+requirements_file = os.path.join(setup_directory, 'requirements.txt')
+__version__ = None
+
+# load long description for PyPi
+with open(readme_file, encoding='utf-8', mode='r') as f:
+    __long_description__ = f.read()
+
+# get version number
+with open(version_file, encoding='utf-8', mode='r') as fd:
+    exec(fd.read())
+
+# get dependencies
+with open(requirements_file, encoding='utf-8', mode='r') as f:
+    __install_requires__ = [i for i in f.read().strip().split('\n')]
+
+# PyPi classifiers
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Education",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Scientific/Engineering",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Operating System :: MacOS",
+]
 
 setup(
     name="pyloa",
-    version="1.0.1",
+    version=__version__,
     license="MIT",
     author="PyLoa Developers",
     author_email="pyloadevelopers@gmail.com",
@@ -17,9 +48,10 @@ setup(
     packages=['pyloa'] + ['pyloa.' + i for i in find_packages('pyloa')],
     provides=['pyloa'],
     python_requires='>=3.5',
-    install_requires=['matplotlib==3.0.3', 'scipy==1.2.1', 'tensorflow==1.13.1', 'tqdm==4.31.1', 'numpy==1.16.2'],
+    classifiers=__classifiers__,
+    install_requires=__install_requires__,
     description="PyLoa: Learning on-line Algorithms with Python",
-    long_description=long_description,
+    long_description=__long_description__,
     long_description_content_type='text/markdown',
     url="https://github.com/pyloa/PyLoa",
     project_urls={

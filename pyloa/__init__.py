98b98bf1a64e287631d517a8f02dff5bd4d282d7
Matthias Rozanski <matthias.rozanski@ifu.rwth-aachen.de>
2019-07-13 16:42:26 +0200

    removed incorrect __all__

--- pyloa/__init__.py
+++ pyloa/__init__.py
@@ -1,4 +1,2 @@
 from ._main import __author__,__copyright__ , __credits__, __license__, __version__, __maintainer__, __email__, \
     __status__
-
-__all__ = [__author__, __copyright__, __credits__, __license__, __version__, __maintainer__, __email__, __status__]
